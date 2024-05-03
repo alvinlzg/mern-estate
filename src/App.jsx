28702c797ffda2b38e9e4fe47f62a8f36eef459f
alvin <alvin.limzhiguang@resmed.com.au>
2024-05-03 23:46:20 +0800

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>Hello world!</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+    </Routes> 
+
+  </BrowserRouter>
 }
